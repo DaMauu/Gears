cdd7fa34d875f57ef23d2b6849ab7f2e4a5542ba
oiozi <20555213+oiozi@users.noreply.github.com>
2023-10-10 01:16:58 +0800

    Update geo_location2.js

--- Package/QuantumultX/geo_location2.js
+++ Package/QuantumultX/geo_location2.js
@@ -6,9 +6,7 @@
 
   **/
 
-// var content= `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold">` + response.body + `</p>`;
-
-  var urlIPv4 = "https://api.ip.sb/ip";
+  var urlIPv4 = "https://api.ip.sb/geoip";
   var urlIPv6 = "https://api-ipv6.ip.sb/geoip";
   var opts = {
       policy: $environment.params
@@ -16,31 +14,32 @@
   var myRequestIPv4 = {
       url: urlIPv4,
       opts: opts,
-      timeout: 4000
+      timeout: 2000  // 修改这里的超时时间，单位是毫秒
   };
   var myRequestIPv6 = {
       url: urlIPv6,
       opts: opts,
-      timeout: 4000
+      timeout: 2000  // 修改这里的超时时间，单位是毫秒
   };
  
   var message = ""
   const paras = ["ip","isp","country_code","city"]
-  const paran = ["连接方式","ISP","地区","城市"]
+  const paran = ["IP","ISP","地区","城市"]
+  
   $task.fetch(myRequestIPv4).then(responseIPv4 => {
     $task.fetch(myRequestIPv6).then(responseIPv6 => {
-      var ipv4 = responseIPv4? responseIPv4.body.trim() : "";
-      var ipv6 = responseIPv6? json2info(responseIPv6.body, paras).trim() : "";
+      var ipv4 = responseIPv4 ? json2info(responseIPv4.body, paras, "IPv4") : "";
+      var ipv6 = responseIPv6 ? json2info(responseIPv6.body, paras, "IPv6") : "";
       if (ipv4 && ipv6) {
-          message = "IPv4: " + ipv4 + "<br>IPv6: " + ipv6;
+          message = ipv4 + "<br>" + ipv6;
       } else if (ipv4) {
-          message = "IPv4: " + ipv4;
+          message = ipv4;
       } else if (ipv6) {
-          message = "IPv6: " + ipv6;
+          message = ipv6;
       } else {
           message = "无法检测到网络连接信息";
       }
-      $done({"title": "    🔎 IP.SB 查询结果", "htmlMessage": message});
+      $done({"title": "🔎 IP.SB 查询结果", "htmlMessage": message});
     }, reason => {
       message = "</br></br>🛑 IPv6 查询超时"
       message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;">` + message + `</p>`
@@ -53,7 +52,7 @@
   });
 
 
-function json2info(cnt,paras) {
+function json2info(cnt, paras, ipType) {
   var res = "------------------------------"
   cnt = JSON.parse(cnt)
   for (i=0;i<paras.length;i++) {
@@ -62,7 +61,7 @@ function json2info(cnt,paras) {
     }
     res = cnt[paras[i]] ?   res + "</br><b>" + "<font  color=>" + paran[i] + "</font> : " + "</b>" + "<font  color=>" + cnt[paras[i]] + "</font></br>" : res
   }
-  res =res+ "------------------------------"+"</br>"+"<font color=#6959CD>"+"<b>节点</b> ➟ " + $environment.params+ "</font>"
-  res =  `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + res + `</p>`
+  res = res + "------------------------------" + "</br>" + "<font color=#6959CD>" + "<b>节点</b> ➟ " + $environment.params + "</font>"
+  res = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">` + ipType + ": " + res + `</p>`
   return res
 }
